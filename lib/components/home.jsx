f7356088d9a2d683b93244b60eb796bf76886128
Md Ahsan Habib <ah@inov.io>
2016-01-01 03:06:23 +0600

    -first commit and push

---
+++ lib/components/home.jsx
@@ -0,0 +1,15 @@
+
+Home = React.createClass({
+    render() {
+        return (
+            <div className="container">
+                <div className="text-center">
+                    <h1>Wellcome to boilerplate!</h1>
+                    <p>
+                        Flow Router, React, Bootstrap
+                    </p>
+                </div>
+            </div>
+        );
+    }
+});
